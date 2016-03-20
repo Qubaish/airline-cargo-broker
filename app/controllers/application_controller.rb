8a8e238a87d4afcb628a8abda0399c2847201c76
Uzair <uzair@gems.techverx.com>
2016-03-20 16:47:18 +0500

    Make changes in home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :broker_inquiries
 
   def banner_nav
     @banner_nav = true
+  end
+
+  def broker_inquiries
+    @broker_quotes_home = BrokerQuote.all.limit(5)
+  end
+
+  def airplane_inquiries
+    @airplane_quotes_home = Airline.all.limit(5)
   end
 
 end
