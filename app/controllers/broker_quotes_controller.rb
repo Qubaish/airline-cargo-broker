0618570a0e817b7d98b6f3ba753462aa2d0ec829
Uzair <uzair@gems.techverx.com>
2016-03-27 23:52:15 +0500

    implement new broker dashboard design

--- app/controllers/broker_quotes_controller.rb
+++ app/controllers/broker_quotes_controller.rb
@@ -8,7 +8,7 @@ class BrokerQuotesController < ApplicationController
     @quote = BrokerQuote.new(broker_quote_params)
     @quote.user = current_user
     if @quote.save
-      redirect_to root_url
+      redirect_to "/broker_board"
     else
       render :text => "abc"
     end
@@ -30,6 +30,8 @@ class BrokerQuotesController < ApplicationController
   private
 
   def broker_quote_params
+    date = params["day"] + "-" + params["month"] + "-" + params["year"]
+    params[:broker_quote][:date_required] = Date.parse(date)
     params.require(:broker_quote).permit(:from_airpot, :to_airpot, :date_required, :nature_of_cargo, :un_number, :payload, :volume, :cargo_dimension_width, :cargo_dimension_height, :cargo_dimension_length, :quantiy, :user_id, :name, :company_name, :email, :uan_number, :hear, :additional_information, :special_requirements)
   end
 
